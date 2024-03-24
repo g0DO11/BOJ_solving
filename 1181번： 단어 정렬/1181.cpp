82d4899ed0165e62cd75a5813147634929896c13
g0DO11 <inhyungshin@gmail.com>
2024-03-25 04:55:31 +0900

    BOJ-EX: 3/25/2024, 4:55:30 AM

--- 1181번： 단어 정렬/1181.cpp
+++ 1181번： 단어 정렬/1181.cpp
@@ -9,3 +9,26 @@
 // undefSolved: 2024/03/25 04:41:27 by spiders22     ###          ###   ##.kr           undefined
 // undefined                                                                            undefined
 // undefined ************************************************************************** undefined
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int N=0;
+string A[20202];
+string s;
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin>>N;
+    for(int i=0;i<N;i++) cin>>A[i];
+    sort(A, A+N, [&](string a, string b){
+        if(a.size()==b.size()) return a < b;
+        return a.size() < b.size();
+    });
+    for(int i=0;i<N;i++) {
+        if(s!=A[i]) cout<<A[i]<<'\n';
+        s=A[i];
+    }
+    return 0;
+}
