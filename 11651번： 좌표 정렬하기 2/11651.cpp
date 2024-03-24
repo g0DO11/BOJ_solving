c9d0e46bb61b4173e4a3b81deda42a742d4097e3
g0DO11 <inhyungshin@gmail.com>
2024-03-25 04:23:24 +0900

    BOJ-EX: 3/25/2024, 4:23:23 AM

--- 11651번： 좌표 정렬하기 2/11651.cpp
+++ 11651번： 좌표 정렬하기 2/11651.cpp
@@ -1,14 +1,15 @@
-undefined ************************************************************************** undefined
-undefined                                                                            undefined
-undefined                                               :::    :::    :::            undefined
-undefProblem Number: 11651                             :+:    :+:      :+:           undefined
-undefined                                             +:+    +:+        +:+          undefined
-undefBy: spiders22 <boj.kr/u/spiders22>              +#+    +#+          +#+         undefined
-undefined                                           +#+      +#+        +#+          undefined
-undefhttps://boj.kr/11651                          #+#        #+#      #+#           undefined
-undefSolved: 2024/03/25 04:06:04 by spiders22     ###          ###   ##.kr           undefined
-undefined                                                                            undefined
-undefined ************************************************************************** undefined
+/*undefined ************************************************************************** undefined*/
+/*undefined                                                                            undefined*/
+/*undefined                                               :::    :::    :::            undefined*/
+/*undefProblem Number: 11651                             :+:    :+:      :+:           undefined*/
+/*undefined                                             +:+    +:+        +:+          undefined*/
+/*undefBy: spiders22 <boj.kr/u/spiders22>              +#+    +#+          +#+         undefined*/
+/*undefined                                           +#+      +#+        +#+          undefined*/
+/*undefhttps://boj.kr/11651                          #+#        #+#      #+#           undefined*/
+/*undefSolved: 2024/03/25 04:07:55 by spiders22     ###          ###   ##.kr           undefined*/
+/*undefined                                                                            undefined*/
+/*undefined ************************************************************************** undefined*/
+ 
 
 #include <iostream>
 #include <algorithm>
@@ -24,7 +25,7 @@ int main(){
     for(int i=0;i<N;i++) cin>>A[i].first>>A[i].second;
     sort(A, A+N, [&](pair<int, int> a, pair<int, int> b){
         if(a.second==b.second) return a.first < b.first;
-        return a.second < b.second;
+        return a.second < b.second; 
     });
     for(int i=0;i<N;i++) cout<<A[i].first<<' '<<A[i].second<<'\n';
     return 0;
